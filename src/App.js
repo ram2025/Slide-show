bd2cdbf88306d94d0a088177d63505bc97f43726
ram2025 <89303952+ram2025@users.noreply.github.com>
2022-07-12 21:59:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import "./styles.css";
+
+import { useState } from "react";
+
+function One() {
+  return (
+    <>
+      <h1>Page-1</h1>
+      <p>First Slide</p>
+    </>
+  );
+}
+
+function Two() {
+  return (
+    <>
+      <h1>Page-2</h1>
+      <p>Second Slide</p>
+    </>
+  );
+}
+
+function Three() {
+  return (
+    <>
+      <h1>Page-3</h1>
+      <p>Third Slide</p>
+    </>
+  );
+}
+
+function Four() {
+  return (
+    <>
+      <h1>Page-4</h1>
+      <p>Fourth Slide</p>
+    </>
+  );
+}
+export default function App() {
+  const [state, newState] = useState(1);
+  return (
+    <div className="App">
+      <div className="box">
+        <div className="content">
+          {state === 1 ? (
+            <One />
+          ) : state === 2 ? (
+            <Two />
+          ) : state === 3 ? (
+            <Three />
+          ) : state === 4 ? (
+            <Four />
+          ) : null}
+        </div>
+        <div className="btn">
+          {state !== 1 ? (
+            <button
+              onClick={() => {
+                newState(state - 1);
+              }}
+            >
+              Back
+            </button>
+          ) : null}
+          {state !== 4 ? (
+            <button
+              onClick={() => {
+                newState(state + 1);
+              }}
+            >
+              Next
+            </button>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+}
